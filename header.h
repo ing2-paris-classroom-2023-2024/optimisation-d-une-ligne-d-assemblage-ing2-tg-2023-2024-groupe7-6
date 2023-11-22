94ce5930ddef7d926abf53c2256e35b9213c34e3
Hugo <hugo2.tat@gmail.com>
2023-11-22 14:31:46 +0100

    ajout des structures pour stocker les information sur les operations et sur les station. Hugo

--- header.h
+++ header.h
@@ -1,8 +1,21 @@
-//
-// Created by quent on 22/11/2023.
-//
 
 #ifndef PROJET_ING_2_HEADER_H
 #define PROJET_ING_2_HEADER_H
+
+
+//Structure pour stocker les informations sur chaque opération
+typedef struct
+{
+    int numero;
+    double temps_execution;
+}t_Operation;
+
+//Structure pour stocker les informations sur chaque station
+typedef struct
+{
+    int numero;
+    double temps_occupe;
+}t_Station;
+
 
 #endif //PROJET_ING_2_HEADER_H
