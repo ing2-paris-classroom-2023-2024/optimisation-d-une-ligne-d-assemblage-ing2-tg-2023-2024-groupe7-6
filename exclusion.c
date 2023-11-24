216542a213d974585451d133a7e6e79b4f5b6298
romslte <rolhote3@gmail.com>
2023-11-24 21:10:51 +0100

    init exclusion.h et exclusion.c romain 1ère fonction exlusion

--- exclusion.c
+++ exclusion.c
@@ -1,4 +1,12 @@
 //
 // Created by rolho on 20/11/2023.
 //
-
+// Fonction pour vérifier si deux opérations peuvent être affectées à la même station
+bool peutAffecter(int matriceAdjacence[MAX_NOEUDS][MAX_NOEUDS], int affectations[], int op, int station, int nombreNoeuds) {
+    for (int i = 0; i < nombreNoeuds; i++) {
+        if (matriceAdjacence[op][i] && station == affectations[i]) {
+            return false; // Les opérations adjacentes ne peuvent pas être dans la même station
+        }
+    }
+    return true;
+}
